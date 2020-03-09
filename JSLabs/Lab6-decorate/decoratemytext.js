7ad475a4bea0fd8dc23623bb4c6f2909bf45f244
habtu <habtassefa@gmail.com>
2020-03-08 21:50:02 -0600

    decorated js

---
+++ JSLabs/Lab6-decorate/decoratemytext.js
@@ -0,0 +1,78 @@
+window.onload = function(){
+    document.getElementById("bigger").onclick=this.timerBigger; 
+    document.getElementById('igpay').onclick = this.igpayAninlay; 
+    document.getElementById('malkovitch').onclick = this.malkovitch; 
+
+}
+timer = null; 
+function timerBigger(){
+    if(timer === null)
+        timer = setInterval(bigger, 1000); 
+    else{
+        clearInterval(timer); 
+        timer = null; 
+    }
+}
+function bigger(){
+  var size = parseInt(document.getElementById("my-text").style.fontSize)
+   console.log("current size "+size)
+   if(!size){
+       size = 12; 
+   }
+   size += 2; 
+   console.log(size)
+  document.getElementById("my-text").style.fontSize = size +"pt"; 
+
+}
+
+function decorate(){
+    var chBox = document.getElementById("myCheck"); 
+
+    if(chBox.checked){
+        document.getElementById("my-text").style.fontWeight = "bold";
+        document.getElementById("my-text").style.textDecoration = "underline"; 
+        document.getElementById("my-text").style.color = "green"; 
+        document.getElementById("mainBody").style.backgroundImage= "url('../img/hundred-dollar-bill.jpg')"
+
+    }
+    else{
+        document.getElementById("my-text").style.fontWeight = "normal";
+        document.getElementById("my-text").style.textDecoration = "none"; 
+        document.getElementById("my-text").style.color = "black"; 
+        document.getElementById("mainBody").style.backgroundImage=""; 
+    }
+
+}
+
+function igpayAninlay(){
+  
+    var text = document.getElementById("my-text").value; 
+    var textArray = []; 
+     textArray = text.split(" ");
+
+     for(word in textArray){
+         console.log(textArray[word])
+     }
+
+    console.log(textArray)
+}
+
+function malkovitch(){
+    console.log("malkovitch")
+    var text = document.getElementById("my-text").value; 
+    var textArray = []; 
+    var changedArray = []
+     textArray = text.split(" ");
+
+
+    for(word in textArray){
+        var nextword = textArray[word]; 
+        if(nextword.length >= 5){
+            nextword = "Malkovitch"
+        }
+        
+        changedArray.push(nextword)
+    }    
+
+    document.getElementById("my-text").value = changedArray.join(" "); 
+}
